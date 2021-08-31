83b8ea39e28ba0d9cf6d95e444db0cbd05013107
jame123785656 <624259022@webmail.npru.ac.th>
2021-08-31 18:26:16 +0700

    first commit

---
+++ midterm/src/App.js
@@ -0,0 +1,23 @@
+import Naverbar from "./page/Naverbar"
+import Header from "./page/Header"
+import Homepage from "./page/Homepage"
+import Footer from "./page/Footer";
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+   
+              <Naverbar />
+              <Header />
+              <Homepage />
+              <Footer />
+            
+    </div>
+  );
+}
+
+export default App;
